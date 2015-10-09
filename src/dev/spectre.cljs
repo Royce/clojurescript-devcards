7862e0b9a06ba14bc76f159970b1921da944a5aa
Royce Townsend <git@roycetownsend.com>
2015-10-09 16:12:37 +0800

    Improve application of mask and capture error message.

--- src/dev/spectre.cljs
+++ src/dev/spectre.cljs
@@ -17,29 +17,43 @@
 
 (def masks {[:form :age] (numeric-string)
             [:form :phone] phone-number-mask})
+;; (def required {[:form]})
 
+(defn new-state [state [_ path value] {:keys [masks]}]
+  (let [mask (condition (vlad/present) (masks path))
+        mask-validation (vlad/validate (or mask vlad/valid) value)
+        state (if (empty? mask-validation)
+                (setval path value state)
+                (setval [:warning]
+                        (map vlad.core/english-translation mask-validation)
+                        state))]
+    state))
 
+(defcard n (new-state {:form {:age "12" :phone "95359513"}}
+                      [:setval [:form :age] "a"]
+                      {:masks masks}))
 
 
 (defonce state (atom {:_counter 0
                       :heading "The heading"
-                      :form {:name "Jack" :age "" :phone ""}}))
+                      :form {:name "Jack" :age "" :phone ""}
+                      :_warning {:form {:age "Too young"}}
+                      :_error {}}))
+
 (defonce bus
   (let [ch (async/chan)]
     (go-loop []
-             (let [[_ path v] (async/<! ch)
-                   valid (vlad/valid? (or (masks path) vlad/valid) v)]
-               (println [_ path v])
-               (swap! state (fn [state]
-                              (if valid
-                                (setval path v state)
-                                (update state :_counter inc)))))
+             (let [msg (async/<! ch)]
+               (println msg)
+               (swap! state #(new-state % msg {:masks masks})))
              (recur))
     ch))
 
 
 (rum/defc text < rum/static [state path bus {:keys [label]}]
-  (let [id (clojure.string/join path)]
+  (let [id (clojure.string/join path)
+        error   (first (select path (state :error)))
+        warning (first (select path (state :warning)))]
     [:div
      [:label {:for id} label]
      [:input {:id id
@@ -47,7 +61,8 @@
               :on-change
               (fn [e]
                 (.preventDefault e)
-                (async/put! bus [:setval path (.. e -target -value)]))}]]))
+                (async/put! bus [:setval path (.. e -target -value)]))}]
+     (if error error (if warning warning))]))
 
 (rum/defc ui < rum/static [state bus]
   [:div
