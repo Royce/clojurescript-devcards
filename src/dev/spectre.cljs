2b6e7dadcf6b262692371c880ec80b7fa3362614
Royce Townsend <git@roycetownsend.com>
2015-10-10 12:08:19 +0800

    Try to remove a key from a nested map

--- src/dev/spectre.cljs
+++ src/dev/spectre.cljs
@@ -1,6 +1,6 @@
 (ns dev.spectre
   (:require
-   [com.rpl.specter :as s :refer [select setval ALL]]
+   [com.rpl.specter :as s :refer [select setval transform putval]]
    [cljs.test])
   (:require-macros
    [cljs.test :refer [is testing]]
@@ -21,18 +21,29 @@
                    state
                    ))
 
-(defcard select (select path
-                   {:form {:age "12"
-                           :phone "95359513"}}
-                   ))
+(defcard select (select path state))
 
 (defcard set (setval (vec (concat [:_warning] path))
-                   "Error message"
-                   {:form {:age "12"
-                           :phone "95359513"}}
+                     "Error message"
+                     state))
+
+(defcard select (select
+                 (vec (concat [:_warning] path))
+                 state))
+
+(defcard-doc "state" (dc/mkdn-pprint-source transform))
+
+(defcard remove (transform [:_warning :form (putval :age)]
+                        #(dissoc %2 %1)
+                        {:_warning {:form {:age "Error message"}}
+                         :form {:age "12"
+                                :phone "95359513"}}
                    ))
 
-(defcard select (select (vec (concat [:_warning] path))
-                   {:form {:age "12"
-                           :phone "95359513"}}
-                   ))
+(defcard remove (update-in
+                 {:_warning {:form {:age "Error message"}}
+                  :form {:age "12"
+                         :phone "95359513"}}
+                 [:_warning :form]
+                 dissoc :age
+                 ))
